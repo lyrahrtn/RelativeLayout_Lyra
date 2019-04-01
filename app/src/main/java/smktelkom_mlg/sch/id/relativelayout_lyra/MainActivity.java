66c0558053d11e671ea02b0700d36b44dcdd4581
lyrahrtn <lyra_hertin_26rpl@student.smktelkom-mlg.sch.id>
2019-04-01 14:15:14 +0700

    Selesai

---
+++ app/src/main/java/smktelkom_mlg/sch/id/relativelayout_lyra/MainActivity.java
@@ -0,0 +1,13 @@
+package smktelkom_mlg.sch.id.relativelayout_lyra;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
